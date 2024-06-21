0e5bb54b93728b469a468690d269be068c115162
Daan0101 <173400@student.horizoncollege.nl>
2024-06-21 11:12:54 +0200

    Update(navbar.php) styling

--- fieldlabs/includes/navbar.php
+++ fieldlabs/includes/navbar.php
@@ -1,7 +1,9 @@
 <nav class="sidenav">
     <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="contact.php">Contact</a></li>
+        <li><div class="row"><a href="../fieldlabs/index.php"><i class="fa-solid fa-house"></i>Home</a></div></li>
+        <li><div class="row"><a href="../fieldlabs/search.php"><i class="fa-solid fa-magnifying-glass"></i>Search</a></div></li>
+        <li><div class="row"><a href="../fieldlabs/post.php"><i class="fa-solid fa-pen-to-square"></i>Post</a></div></li>
     </ul>
 </nav>
+
+
