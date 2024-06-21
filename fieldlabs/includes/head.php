251ed9d072296f4740dce73b2c03b9267ac54695
Daan0101 <173400@student.horizoncollege.nl>
2024-06-21 09:39:17 +0200

    Update(head.php) Session start en database connectie voor de head

--- fieldlabs/includes/head.php
+++ fieldlabs/includes/head.php
@@ -17,3 +17,14 @@
 <!-- Fontawesome -->
 <script src="https://kit.fontawesome.com/b801578fa3.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+<!-- PHP Session and connection with database -->
+
+<?php
+
+session_start();
+
+include("./functions/functions.php");
+//$conn = dbConnect();
+
+?>
