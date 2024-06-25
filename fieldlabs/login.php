5519b15c0c9308c509af68ef0c3c99aabb4f2117
Daan0101 <173400@student.horizoncollege.nl>
2024-06-25 10:45:26 +0200

    Feature(Login/Register.php) Login en registreren

---
+++ fieldlabs/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+
+    include ("./includes/head.php");
+
+    ?>
+</head>
+
+<body>
+
+    <?php
+    include ("./includes/header.php");
+    ?>
+
+    <?php
+    include ("./includes/navbar.php");
+    ?>
+
+    <div class="container">
+
+        <div class="main-content">
+
+            <!-- Login content -->
+            <h1>Login content</h1>
+
+        </div>
+    </div>
+
+    <?php
+    include ("./includes/footer.php");
+    ?>
+</body>
---
+++ fieldlabs/register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+
+    include ("./includes/head.php");
+
+    ?>
+</head>
+
+<body>
+
+    <?php
+    include ("./includes/header.php");
+    ?>
+
+    <?php
+    include ("./includes/navbar.php");
+    ?>
+
+    <div class="container">
+
+        <div class="main-content">
+
+            <!-- Register content -->
+            <h1>Register</h1>
+
+        </div>
+    </div>
+
+    <?php
+    include ("./includes/footer.php");
+    ?>
+</body>
