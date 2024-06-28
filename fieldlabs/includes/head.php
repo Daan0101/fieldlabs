f2005547f45d3e8cf1d9f155128aac6bdc5201b0
Damienvdhelm <damienvdhelm@gmail.com>
2024-06-28 11:43:53 +0200

    feature(favicon) favicon toegevoegd

--- fieldlabs/includes/head.php
+++ fieldlabs/includes/head.php
@@ -18,6 +18,9 @@
 <script src="https://kit.fontawesome.com/b801578fa3.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
+<!-- Favicon -->
+<link rel="icon" type="image/x-icon" href="../fieldlabs/assets/images/favicon.ico">
+
 <!-- PHP Session and connection with database -->
 
 <?php
