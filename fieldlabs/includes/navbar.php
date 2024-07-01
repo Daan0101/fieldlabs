edbf5405b43bf91f03dd55a5dd347198e08a68e8
Damienvdhelm <damienvdhelm@gmail.com>
2024-07-01 15:28:17 +0200

    style.css(navbar.php) de username een style gegeven

--- fieldlabs/includes/navbar.php
+++ fieldlabs/includes/navbar.php
@@ -16,7 +16,7 @@
             <li><div class="navRow"><a href="../fieldlabs/post.php"><i class="fa-solid fa-pen-to-square"></i>Post</a></div></li>
             <li><div class="navRow"><a href="../fieldlabs/myGroups.php"><i class="fa-solid fa-users"></i>my groups</a></div></li>
             <li><div class="navRow"><a href="../fieldlabs/logout.php"><i class="fa-solid fa-right-from-bracket"></i>Logout</a></div></li>
-            <li><div class="navRow"><a href="../fieldlabs/logout.php"><i class="fa-solid fa-right-from-bracket"></i>'.$username.'</a></div></li>
+            <li><div class="navRow"><p class="username">Username: ' . $username . '</p></div></li>
             ';
         } else {
             echo '
