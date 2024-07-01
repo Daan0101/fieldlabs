32f00eef84705165a54ab45ac9ad1b2d5cf67412
Appie Khalid <174663@student.horizoncollege.nl>
2024-07-01 15:20:17 +0200

    feat ( navbar.php) Je ziet nu je gebruikersnaam in de navbar.

--- fieldlabs/includes/navbar.php
+++ fieldlabs/includes/navbar.php
@@ -3,12 +3,20 @@
         <?php
 
         if (isset($_SESSION['uid'])) {
+            $query = "SELECT * FROM users WHERE uid = :uid";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam('uid', $_SESSION['uid']);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $data = $stmt->fetch();
+            $username = $data['username'];
             echo '
             <li><div class="navRow"><a href="../fieldlabs/index.php"><i class="fa-solid fa-house"></i>Home</a></div></li>
             <li><div class="navRow"><a href="../fieldlabs/search.php"><i class="fa-solid fa-magnifying-glass"></i>Search</a></div></li>
             <li><div class="navRow"><a href="../fieldlabs/post.php"><i class="fa-solid fa-pen-to-square"></i>Post</a></div></li>
             <li><div class="navRow"><a href="../fieldlabs/myGroups.php"><i class="fa-solid fa-users"></i>my groups</a></div></li>
             <li><div class="navRow"><a href="../fieldlabs/logout.php"><i class="fa-solid fa-right-from-bracket"></i>Logout</a></div></li>
+            <li><div class="navRow"><a href="../fieldlabs/logout.php"><i class="fa-solid fa-right-from-bracket"></i>'.$username.'</a></div></li>
             ';
         } else {
             echo '
