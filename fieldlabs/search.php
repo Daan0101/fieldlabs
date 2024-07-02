63c59b715b47c5cac7a49d737f95c78b0ecb8292
Appie Khalid <174663@student.horizoncollege.nl>
2024-07-02 11:05:22 +0200

    feat ( search.php) Alle posts worden nu laten zien

--- fieldlabs/search.php
+++ fieldlabs/search.php
@@ -15,18 +15,58 @@
     include("./includes/header.php");
     ?>
 
-<?php
+    <?php
     include("./includes/navbar.php");
     ?>
 
-<div class="container">
-    <div class="main-content">
+    <div class="container">
+        <div class="main-content">
 
-        <!-- Search content -->
-        <h1>Search</h1>
+            <!-- Search content -->
+            <!-- <h1>Search</h1> -->
 
+            <?php
+            try {
+                $query = "SELECT * FROM posts";
+                $conn = dbConnect();
+                $stmt = $conn->prepare($query);
+                $stmt->execute();
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                $records = $stmt->fetchAll();
+                foreach ($records as $record) {
+                    // var_dump($record);
+                    $title = $record["title"];
+                    $details =  $record["details"];
+                    $post_id =  $record["post_id"];
+
+                    $url = "searchDetails.php?post_id=$post_id";
+                    // -----------------------------------
+                    $query = "SELECT username FROM users WHERE uid = :uid";
+                    $stmt = dbConnect()->prepare($query);
+                    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                    $stmt->execute(['uid' => $_SESSION['uid']]);
+                    $record = $stmt->fetch();
+                    $username = $record['username'];
+                    // -----------------------------------
+
+            ?>
+                    <div>
+                        <h1>aa<?php echo $title; ?></h1>
+                        <article><?php echo $details; ?></article>
+                        <h5><?php echo $username; ?></h5>
+                        
+                    </div>
+                                            <a href="<?php echo $url?>">Meer details</a>
+            <?php
+                }
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+
+            ?>
+
+        </div>
     </div>
-</div>
 
     <?php
     include("./includes/footer.php");
