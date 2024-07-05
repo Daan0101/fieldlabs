cf70d18bf74e31fb886b317f6c7a8f7db0e9250b
Damienvdhelm <damienvdhelm@gmail.com>
2024-07-05 11:16:00 +0200

    fix(navbar.php) comments gefixed

--- fieldlabs/includes/navbar.php
+++ fieldlabs/includes/navbar.php
@@ -11,26 +11,26 @@
             $data = $stmt->fetch();
             $username = $data['username'];
             $role = $data['role']; 
-            // Display common links for both roles
+            // Navigatie voor beide rollen
             echo '
             <li><div class="navRow"><a href="../fieldlabs/index.php"><i class="fa-solid fa-house"></i>Home</a></div></li>
             <li><div class="navRow"><a href="../fieldlabs/search.php"><i class="fa-solid fa-magnifying-glass"></i>Search</a></div></li>
             <li><div class="navRow"><a href="../fieldlabs/myGroups.php"><i class="fa-solid fa-users"></i>My Groups</a></div></li>
             ';
 
-            // Display links based on user role
+            // Navigatie voor docenten
             if ($role === 'Docent') {
                 echo '
                 <li><div class="navRow"><a href="../fieldlabs/post.php"><i class="fa-solid fa-pen-to-square"></i>Post</a></div></li>
                 ';
             }
-            // Display common links for both roles
+            // Navigatie voor beide rollen
             echo '
             <li><div class="navRow"><a href="../fieldlabs/logout.php"><i class="fa-solid fa-right-from-bracket"></i>Logout</a></div></li>
             <li><div class="navRow"><p class="username">Username: ' . $username . '</p></div></li>
             ';
         } else {
-            // Display links for users not logged in
+            // Links voor degene die niet zijn ingelogd
             echo '
             <li><div class="navRow"><a href="../fieldlabs/register.php"><i class="fa-solid fa-user-plus"></i>Register</a></div></li>
             <li><div class="navRow"><a href="../fieldlabs/login.php"><i class="fa-solid fa-right-to-bracket"></i>Login</a></div></li>
