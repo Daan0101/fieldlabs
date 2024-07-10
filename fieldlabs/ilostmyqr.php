c219db053d4a723e19b62b644ad9c5dc8a0e69cd
Damienvdhelm <damienvdhelm@gmail.com>
2024-07-10 14:44:50 +0200

    styled(ilostmyqr.php) de ilostmyqr page gestyled

--- fieldlabs/ilostmyqr.php
+++ fieldlabs/ilostmyqr.php
@@ -1,15 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<?php
+
+include ("./includes/head.php");
+
+?>
 </head>
 <body>
+<?php
+    include ("./includes/header.php");
+    ?>
+
+    <?php
+    include ("./includes/navbar.php");
+    ?>
+    <div class="main-content">
+
     <form action="" method="post">
         <input type="text" name="token" id="token" placeholder="your token"></input>
-        <input type="submit">
+        <br>
+        <input type="submit" value="submit">
     </form>
+    </div>
+    <?php
+    include ("./includes/footer.php");
+    ?>
 </body>
 </html>
 
