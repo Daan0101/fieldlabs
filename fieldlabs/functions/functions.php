219089b63d33c8c15c5c69a4f4183b064978fe80
Damienvdhelm <damienvdhelm@gmail.com>
2024-07-11 14:46:26 +0200

    update(functions.php) de username in de app is nu de email voor duidelijkheid en daily standup gedaan

--- fieldlabs/functions/functions.php
+++ fieldlabs/functions/functions.php
@@ -159,7 +159,7 @@ function register($conn)
 
             $cURLConnection = curl_init();
 
-            $url = 'https://www.authenticatorapi.com/pair.aspx?AppName=Fieldlabs&AppInfo=' . $username . '&SecretCode=' . $token;
+            $url = 'https://www.authenticatorapi.com/pair.aspx?AppName=Fieldlabs&AppInfo=' . $email . '&SecretCode=' . $token;
             curl_setopt($cURLConnection, CURLOPT_URL, $url);
 
             curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
