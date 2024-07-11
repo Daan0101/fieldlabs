888de2992addd53ff13b8fcf90aa56a4d370d193
Damienvdhelm <damienvdhelm@gmail.com>
2024-07-11 14:37:25 +0200

    fix(admin.php) als de user geen admin is dan kan hij / zij niet bij deze pagina

--- fieldlabs/admin.php
+++ fieldlabs/admin.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php include ("./includes/head.php"); ?>
 </head>
+
 <body>
     <?php include ("./includes/header.php"); ?>
     <?php include ("./includes/navbar.php"); ?>
     <div class="container">
+        <!-- Check of de user een admin is -->
+        <?php
+        if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+            header("Location: ./login.php");
+            exit();
+        }
+
+        ?>
         <!-- Main content -->
         <div class="main-content">
             <form action="" method="post">
-                
+
                 <select name="username" id="username">
                     <option value="none" selected disabled>Selecteer een gebruiker</option>
                     <?php
@@ -43,10 +53,10 @@
                     $role = $_POST['role'];
                     $query = "UPDATE users SET role = :role WHERE uid = :uid";
                     $stmt = dbConnect()->prepare($query);
-                    $stmt->execute([':role' => $role, ':uid' => $uid]);  
-    
+                    $stmt->execute([':role' => $role, ':uid' => $uid]);
+
                     echo "<script type=\"text/javascript\">toastr.success('The user has been updated')</script>";
-                    
+
                 }
             } catch (PDOException $e) {
                 echo $e->getMessage();
@@ -56,4 +66,5 @@
     </div>
     <?php include ("./includes/footer.php"); ?>
 </body>
+
 </html>
